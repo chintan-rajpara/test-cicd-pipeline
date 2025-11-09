4bc59be00840050e8e45eda6b31b6d2f0c0680ce
chintan3710 <chintanrajpara34@gmail.com>
2025-11-09 12:11:04 +0530

    fix: move server listen logic inside main module check

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => res.send('Hello CI/CD World!'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+
+if (require.main === module) {
+    app.listen(3000, () => console.log('Server running on port 3000'));
+}
+
 module.exports = app;
